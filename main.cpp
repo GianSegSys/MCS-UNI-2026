5b0c55a80aa159cc7e85100e178d83fe59ef154b
GianSegSys <gsegoviaq@gmail.com>
2026-01-31 17:21:49 -0600

    main.cpp updated

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
-// 
+//
 int main(){
-    std::cout << "MCS-2026\n";
+    std::cout << "MCS-2026\n\n";
+    std::cout << "Course: Advanced Programming\n";
+    std::cout << "Instructor: Ernesto Cuadros-Vargas\n";
     return 0;
 }
